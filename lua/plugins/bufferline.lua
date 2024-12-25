95b7977f3ddd6712b37d1a602041ee8e443fd4ac
Yao-Xinchen <yao29@illinois.edu>
2024-12-24 15:55:52 -0900

    keep bufferline always on

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+    {
+      "akinsho/bufferline.nvim",
+      config = function()
+        require("bufferline").setup {
+          options = {
+            always_show_bufferline = true,
+          }
+        }
+      end
+    }
+  }
