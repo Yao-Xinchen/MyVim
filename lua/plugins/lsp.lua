3185533ccbb2b43b798096bfe56c96003a945068
Yao-Xinchen <yao29@illinois.edu>
2025-01-17 16:20:37 -0600

    change keymap for completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,21 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {
-        servers = {
-            pyright = {}, -- Enable pyright
-            matlab_ls = {},
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                pyright = {}, -- Enable pyright
+                matlab_ls = {},
+            },
+        },
+    },
+
+    {
+        "blink.cmp",
+        opts = {
+            keymap = {
+                ["<CR>"] = {},
+                ["<Tab>"] = { "accept" },
+            },
         },
     },
 }
