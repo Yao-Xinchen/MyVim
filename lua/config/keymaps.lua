e6a9799c3435bc1c96834d4a641d6baabe339ba9
Yao-Xinchen <yao29@illinois.edu>
2025-01-18 13:30:14 -0600

    change keymap for terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,14 +3,28 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
+-- delete buffer
 map("n", "W", function()
     Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
 
-vim.keymap.set('n', '<C-M-l>', function()
+-- terminal
+map("n", "<c-`>", function()
+    Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+
+map("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+unmap("n", "<C-/>")
+unmap("t", "<C-/>")
+
+-- format
+map('n', '<C-M-l>', function()
     vim.lsp.buf.format({ async = true })
 end, { noremap = true, silent = true })
 
+-- indent
 vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
