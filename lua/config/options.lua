cead26185128e5a20012af6e5b648ead2324be0f
Yao-Xinchen <yao29@illinois.edu>
2025-01-23 20:26:24 -0600

    enable auto restore last session

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,13 +5,13 @@
 -- vim.opt.winbar = "%=%m %f"
 
 -- Set tab width to 4 spaces
-vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
-vim.opt.shiftwidth = 4 -- Number of spaces for each indentation level
-vim.opt.softtabstop = 4 -- Number of spaces for a <Tab> when editing
-vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 4            -- Number of spaces a <Tab> counts for
+vim.opt.shiftwidth = 4         -- Number of spaces for each indentation level
+vim.opt.softtabstop = 4        -- Number of spaces for a <Tab> when editing
+vim.opt.expandtab = true       -- Convert tabs to spaces
 
 vim.opt.relativenumber = false -- Disable relative line numbers
-vim.opt.number = true -- Enable absolute line numbers
+vim.opt.number = true          -- Enable absolute line numbers
 
 local bg_color = "#1e1e2e"
 vim.api.nvim_set_hl(0, "BufferLineFill", { bg = bg_color }) -- Set the background
--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -9,8 +9,8 @@ return {
         lazy = false,
         keys = {
             -- Will use Telescope if installed or a vim.ui.select picker otherwise
-            { "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
-            { "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+            { "<leader>wr", "<cmd>SessionSearch<CR>",         desc = "Session search" },
+            { "<leader>ws", "<cmd>SessionSave<CR>",           desc = "Save session" },
             { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
         },
 
@@ -18,6 +18,9 @@ return {
         ---@module "auto-session"
         ---@type AutoSession.Config
         opts = {
+            auto_restore_last_session = true,
+            lazy_support = true,
+
             suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/", "~/Dev" },
 
             -- ⚠️ This will only work if Telescope.nvim is installed
