a621493ade1dc7e117b3df90c33588f09bb88e7d
Yao-Xinchen <yao29@illinois.edu>
2025-01-24 18:34:08 -0600

    add bufferline indetn

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,6 +5,14 @@ return {
             require("bufferline").setup({
                 options = {
                     always_show_bufferline = true,
+                    offsets = {
+                        {
+                            filetype = "neo-tree",
+                            text = "NeoTree",
+                            highlight = "Directory",
+                            text_align = "left",
+                        },
+                    },
                 },
             })
         end,
